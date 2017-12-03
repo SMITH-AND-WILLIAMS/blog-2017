73ef1aaeefc27ce03a61dd8f760900e8eeaa3618
Gilles BERTRAND <gilles@triptyk.eu>
2017-12-03 15:04:50 +0100

    contact page

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -1,10 +1,24 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-
-    actions: {       
+    focused:false,
+    
+    actions: {      
+        
+        focus(state,val,name) {
+            // //set focused true or false
+            this.set('focused', state);
+            //if not focused anymore but input not empty
+             if(!state && val !=""){
+                //force state
+                state=true;
+                this.set('focused', state);
+            }  
+            console.log(state,name);           
+                  
+        },
         submit : function(){
-            console.log("dans le controller");
+            console.log("sent");
         }
     }
 });
